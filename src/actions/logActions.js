3009dd033c7fe8644d096e02be7a776fa87e7006
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2020-03-19 15:58:58 -0400

    Add log action

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,5 +1,6 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+import { GET_LOGS, ADD_LOG, SET_LOADING, LOGS_ERROR } from './types';
 
+// Get logs from server
 export const getLogs = () => async dispatch => {
   try {
     setLoading();
@@ -9,6 +10,32 @@ export const getLogs = () => async dispatch => {
 
     dispatch({
       type: GET_LOGS,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data
+    });
+  }
+};
+
+// Add new log
+export const addLog = log => async dispatch => {
+  try {
+    setLoading();
+
+    const response = await fetch('/logs', {
+      method: 'POST',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+
+    dispatch({
+      type: ADD_LOG,
       payload: data
     });
   } catch (error) {
